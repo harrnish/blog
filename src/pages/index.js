69861d62f098231ded3a3d095dd1e4ad7f0d19b1
harrnish <harrnish@gmail.com>
2023-06-13 23:42:26 -0400

    Querying blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,37 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
 
-export default function Home() {
+export default function Home({ data }) {
+  const blogs = data.allMarkdownRemark.nodes;
   return (
     <Layout>
       <h1>Blog</h1>
+      {blogs.map((blog, index) => (
+        <p key={index}>{blog.frontmatter.fullTitle}</p>
+      ))}
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: ASC }) {
+      nodes {
+        html
+        frontmatter {
+          date
+          desc
+          fullTitle
+          img {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+          slug
+          title
+        }
+      }
+    }
+  }
+`;
