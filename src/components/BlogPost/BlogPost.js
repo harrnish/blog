4147c8052f1e43d7221c7921e481ec5a5f08c7df
harrnish <harrnish@gmail.com>
2023-06-15 21:12:43 -0400

    Date formatting

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -3,6 +3,13 @@ import { Link } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 import * as styles from "./BlogPost.module.css";
 
+function formatDate(date) {
+  let newDate = new Date(date * 1000).toDateString();
+  newDate = newDate.split(" ");
+
+  return newDate[2] + " " + newDate[1] + ", " + newDate[3];
+}
+
 const BlogPost = ({ data }) => {
   return (
     <div className={styles.postContainer}>
@@ -15,7 +22,7 @@ const BlogPost = ({ data }) => {
             quality={100}
           />
         </Link>
-        <p>{data.frontmatter.date}</p>
+        <p>{formatDate(data.frontmatter.date)}</p>
         <Link to={"/blog/" + data.frontmatter.slug}>
           <h1 className={styles.postTitle}>{data.frontmatter.title}</h1>
         </Link>
